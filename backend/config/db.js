53adb227b66b77f70ea605af8d73f03e7406df12
Sadaan Khan Durrani <sadaankhan2346@gmail.com>
2026-03-31 19:24:08 +0500

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-const productSchema = mongoose.createConnection('mongodb://127.0.0.1:27017/products');
-const userSchema = mongoose.createConnection('mongodb://127.0.0.1:27017/user');
-
-module.exports = { productSchema, userSchema};
